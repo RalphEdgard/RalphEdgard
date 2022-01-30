bb62e4da5c268539c5b4eb730e890db20cf35761
RalphEdgard <36280173+RalphEdgard@users.noreply.github.com>
2022-01-30 17:38:44 -0500

    Update tonto.js

--- tonto.js
+++ tonto.js
@@ -1,8 +1,78 @@
-var password = "lol";
+function a() {
+	// adds an element at the end
+	// of list
+	add(element)
+	{
+		// creates a new node
+		var node = new Node(element);
+
+		// to store current node
+		var current;
+
+		// if list is Empty add the
+		// element and make it head
+		if (this.head == null)
+			this.head = node;
+		else {
+			current = this.head;
+
+			// iterate to the end of the
+			// list
+			while (current.next) {
+				current = current.next;
+			}
+
+			// add node
+			current.next = node;
+		}
+		this.size++;
+	}
+}
+
+function b() {
+	// insert element at the position index
+	// of the list
+	insertAt(element, index)
+	{
+		if (index < 0 || index > this.size)
+			return console.log("Please enter a valid index.");
+		else {
+			// creates a new node
+			var node = new Node(element);
+			var curr, prev;
+
+			curr = this.head;
+
+			// add the element to the
+			// first index
+			if (index == 0) {
+				node.next = this.head;
+				this.head = node;
+			} else {
+				curr = this.head;
+				var it = 0;
+
+				// iterate over the list to find
+				// the position to insert
+				while (it < index) {
+					it++;
+					prev = curr;
+					curr = curr.next;
+				}
+
+				// adding an element
+				node.next = curr;
+				prev.next = node;
+			}
+			this.size++;
+		}
+	}
+}
+var asdf = "lol";
    
-while (password != "ohmygod") {
-	password = window.prompt("Enter password: ");
-	if (password == "ohmygod") {
+while (asdf != "ohmygod") {
+	asdf = window.prompt("");
+	if (adsf == "ohmygod") {
 		break;
 	} else {
 		continue;
